979e63d84aa500d67dd2e0a86661ae1e224c9d1a
miaolin1993 <miaolin1993@foxmail.com>
2021-03-15 18:52:44 +0800

    优化数组方法, 精简代码

--- 首字符转换大写/tranStr.js
+++ 首字符转换大写/tranStr.js
@@ -3,13 +3,10 @@
  * */
 function handleStr (str) {
 	// 以空格分隔字符串
-	let list = str.split(/\s/)
 	// 遍历数组 slice0,1获取首字母, slice1获取余下内容  拼接
-	list.forEach((value, index) => {
-		list.push(value.slice(0, 1).toUpperCase() + value.slice(1))
+	let newList = str.split(/\s/).map(value => {
+		return value.slice(0, 1).toUpperCase() + value.slice(1)
 	})
-	// 删除数组长度的一半, 保留的是后追加进去的内容
-	list.splice(0, (list.length / 2))
 	// 将数组转换为字符串
-	return list.join(' ')
+	return newList.join(' ')
 }
